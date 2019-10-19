c56e9d4399cfa639256b1777a57b37e72d295c3b
Duong Dang <dddangmedia@gmail.com>
2019-10-19 22:52:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,12 +12,14 @@ def monopoly
 	monopoly = {
 	  railroads: {}
 	}
-
 end
 
 def monopoly_with_second_tier
-  monopoly
-  monopoly[:railroads] = pieces: 4
+  monopoly = {
+    railroads: {
+      pieces: 4
+    }
+  }
 end
 
 def monopoly_with_third_tier
